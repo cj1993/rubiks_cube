c5110868546dac3c0d8ed1e9ed9b280d64dd308c
Christopher Chandler <christopher.chandler@sky.uk>
2015-09-11 00:29:32 +0100

    Had to split orientation of face and ribbon due to symmetry issues

--- lib/orienter.rb
+++ lib/orienter.rb
@@ -1,12 +1,21 @@
 # Returns an array of all default orientations for the ribbon or face
 
 class Orienter
-  def orient(pattern)
+  def face(face)
     [
-      pattern,
-      pattern.transpose.reverse,
-      pattern.flatten.reverse.each_slice(3).to_a,
-      pattern.reverse.transpose
+      face,
+      face.transpose.reverse,
+      face.flatten.reverse.each_slice(3).to_a,
+      face.reverse.transpose
+    ]
+  end
+
+  def ribbon(ribbon)
+    [
+      ribbon,
+      ribbon.permutation.to_a[5].reverse,
+      ribbon.permutation.to_a[7].reverse,
+      ribbon.permutation.to_a[14].reverse
     ]
   end
 end
