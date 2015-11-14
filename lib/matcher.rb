c96b9a9c01e35aa84737359a03a671c74dabe3ef
Christopher Chandler <christopher.chandler@sky.uk>
2015-11-14 19:21:15 +0000

    Improved move count if alg starts U, U2 or U'

--- lib/matcher.rb
+++ lib/matcher.rb
@@ -5,11 +5,33 @@ class Matcher
     when 0
       algorithm
     when 1
-      "U "  + algorithm
+      optimise_match(index, algorithm.split)
     when 2
-      "U2 " + algorithm
+      optimise_match(index, algorithm.split)
     when 3
-      "U' " + algorithm
+      optimise_match(index, algorithm.split)
+    end
+  end
+
+  private
+
+  def optimise_match(index, alg)
+    case index
+    when 1
+      return "U2 " + alg.drop(1).join(' ') if alg.first == "U"
+      return "U' " + alg.drop(1).join(' ') if alg.first == "U2"
+      return alg.drop(1).join(' ')         if alg.first == "U'"
+      return "U "  + alg.join(' ')
+    when 2
+      return "U "  + alg.drop(1).join(' ') if alg.first == "U'"
+      return "U' " + alg.drop(1).join(' ') if alg.first == "U"
+      return alg.drop(1).join(' ')         if alg.first == "U2"
+      return "U2 " + alg.join(' ')
+    when 3
+      return "U "  + alg.drop(1).join(' ') if alg.first == "U2"
+      return "U2 " + alg.drop(1).join(' ') if alg.first == "U'"
+      return alg.drop(1).join(' ')         if alg.first == "U"
+      return "U' " + alg.join(' ')
     end
   end
 end
